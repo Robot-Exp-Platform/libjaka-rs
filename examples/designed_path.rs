054f7febc278c36b9afa95ab6170144d0fb431b3
SmilingEnchanter <1850200254@qq.com>
2025-05-08 19:16:20 +0800

    绘制矩形和Gcode序列

--- examples/designed_path.rs
+++ examples/designed_path.rs
@@ -16,13 +16,15 @@ struct Point {
 fn main() -> Result<(), Box<dyn Error>> {
     // 1. 初始化机器人
     let mut robot = JakaRobot::new("10.5.5.100");
-    let origin = [-6.0, -298.0, 26.0]; // 基准点
+
+    let origin = [-320.0, 0.0, 60.0]; // 基准点
+
     let sleep_dur = Duration::from_millis(100);
     robot.enable()?;
     robot._stop_program()?;
 
     // 2. 读取 CSV 文件
-    let file = File::open("examples/1.csv")?;
+    let file = File::open("examples/down_sample.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
 
     // 3. 遍历所有坐标点
@@ -37,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         ];
 
         // 执行运动指令
-        robot.move_cartesian_async(&Pose::Euler(target, [180.0, 0.0, 90.0]), 100.0)?;
+        robot.move_cartesian_async(&Pose::Euler(target, [180.0, 0.0, 0.0]), 100.0)?;
 
         // 暂停
         sleep(sleep_dur);
---
+++ examples/move_to_a_point.rs
@@ -0,0 +1,15 @@
+// use std::{thread::sleep, time::Duration};
+
+use libjaka_rs::JakaRobot;
+use robot_behavior::{ArmPreplannedMotionExt, Pose, RobotBehavior, RobotResult};
+
+fn main() -> RobotResult<()> {
+    let mut robot = JakaRobot::new("10.5.5.100");
+    robot.enable()?;
+    robot._stop_program()?;
+    robot.move_cartesian_async(&Pose::Euler([-320.0, 0.0, 50.0], [180.0, 0.0, 0.0]), 100.0)?; // 接触纸面
+    // robot.move_cartesian_async(&Pose::Euler([-320.0, 0.0, 60.0], [180.0, 0.0, 0.0]), 100.0)?; // 离开纸面
+    // robot.move_cartesian_async(&Pose::Euler([-350.0, 0.0, 50.0], [180.0, 0.0, 0.0]), 100.0)?;
+
+    Ok(())
+}
