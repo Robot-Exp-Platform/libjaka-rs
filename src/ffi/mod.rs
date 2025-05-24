29f7fa599604cb56f2af904cb7138c054c1c734d
yixing <yanjizhou312@163.com>
2025-05-24 17:27:54 +0800

    111

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,2 +1,5 @@
 #[cfg(feature = "to_py")]
 mod to_py;
+
+#[cfg(feature = "to_cxx")]
+mod to_cxx;
---
+++ src/ffi/to_cxx.rs
@@ -0,0 +1,11 @@
+// use robot_behavior::RobotBehavior;
+
+// use crate::JakaRobot;
+
+// struct CJakaRobot(JakaRobot);
+
+// impl CJakaRobot {
+//     pub fn create(ip: &str) -> Box<Self> {
+//         Box::new(CJakaRobot(JakaRobot::new(ip)))
+//     }
+// }
