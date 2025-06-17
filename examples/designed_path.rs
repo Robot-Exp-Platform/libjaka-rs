c9852c00a8e9ac862431929dbe210f087b3fc5bc
SmilingEnchanter <1850200254@qq.com>
2025-06-17 21:10:08 +0800

    16周实验

--- examples/designed_path.rs
+++ examples/designed_path.rs
@@ -17,36 +17,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     // 1. 初始化机器人
     let mut robot = JakaRobot::new("10.5.5.100");
 
-    let origin = [-290.0, -57.0, 70.0]; // 基准点
+    let origin = [-290.0, -57.0, 50.0]; // 基准点
 
-    let sleep_dur = Duration::from_millis(100);
+    let sleep_dur = Duration::from_millis(900);
     robot.enable()?;
     robot._stop_program()?;
 
     // 2. 读取 CSV 文件
     let file = File::open("scripts/lines.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
+    robot.move_cartesian_async(&Pose::Euler(origin, [180.0, 0.0, 180.0]), 20.0)?;
+    sleep(Duration::from_millis(500));
 
     // 3. 遍历所有坐标点
-    let mut number = 1;
-    while number != 4 {
-        for result in rdr.deserialize() {
-            let offset: Point = result?; // 解析 CSV 行
+    for result in rdr.deserialize() {
+        let offset: Point = result?; // 解析 CSV 行
 
-            // 计算目标位置 = 基准点 + 偏移量
-            let target = [
-                origin[0] + offset.x,
-                origin[1] + offset.y,
-                origin[2] + offset.z,
-            ];
+        // 计算目标位置 = 基准点 + 偏移量
+        let target = [
+            origin[0] + offset.x,
+            origin[1] + offset.y,
+            origin[2] + offset.z,
+        ];
 
-            // 执行运动指令
-            robot.move_cartesian_async(&Pose::Euler(target, [180.0, 0.0, 180.0]), 20.0)?;
+        // 执行运动指令
+        robot.move_cartesian_async(&Pose::Euler(target, [180.0, 0.0, 180.0]), 10.0)?;
 
-            // 暂停
-            sleep(sleep_dur);
-        }
-        number += 1;
+        // 暂停
+        sleep(sleep_dur);
     }
 
     Ok(())
